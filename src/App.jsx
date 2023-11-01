1ce87aa490415e18f8ee2b3d45b5b2735d6e4168
baitemir <baitemir1706@gmail.com>
2023-11-01 16:53:28 +0600

    added login page and redirect functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import './App.scss'
 import Layout from "./components/Layout/Layout.jsx";
 import {Route, Routes} from "react-router-dom";
+import Redirect from './pages/Redirect/Redirect';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 
 function App() {
     return (
         <Layout>
-            <Routes>
-                <Route index path={"/home"} element={<Hello/>}/>
+            {
+                localStorage.getItem("token")?
+                <Routes>
+                <Route index path={"/home"} element={<Home/>}/>
                 <Route path={"/profile"} element={<Hello2/>}/>
                 <Route path={"/catalog"} element={<Hello3/>}/>
                 <Route path={"/basket"} element={<Hello4/>}/>
             </Routes>
+            :
+            <Routes>
+                <Routes path='*' element={<Redirect/>}/>
+                <Route path='/login' element={<Login/>}/>
+            </Routes>
+            }
         </Layout>
 
     )
