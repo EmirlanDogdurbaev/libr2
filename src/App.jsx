232f83824e380405f8e78ade5e4321a80e5d379a
darkbala <rustam.samsaev@gmail.com>
2023-11-06 12:59:01 +1100

    'something'

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import "./App.scss";
-import Layout from "./components/Layout/Layout.jsx";
-import { Route, Routes } from "react-router-dom";
-import Redirect from "./pages/Redirect/Redirect";
-import Home from "./pages/Home/Home";
-import AuthForm from "./components/AuthForm/AuthForm.jsx";
-import Login from "./pages/Auth/Login.jsx";
-import Register from "./pages/Auth/Register.jsx";
-import Catalog from "./pages/Catalog/Catalog.jsx";
+import './App.scss';
+import Layout from './components/Layout/Layout.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Redirect from './pages/Redirect/Redirect';
+import Home from './pages/Home/Home';
+import AuthForm from './components/AuthForm/AuthForm.jsx';
+import Login from './pages/Auth/Login.jsx';
+import Register from './pages/Auth/Register.jsx';
+import Catalog from './pages/Catalog/Catalog.jsx';
 function App() {
   return (
     <>
-      {localStorage.getItem("token") ? (
+      {localStorage.getItem('token') ? (
         <Layout>
           <Routes>
             <Route index element={<Home />} />
