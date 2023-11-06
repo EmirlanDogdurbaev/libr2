d1e8517aea274cbc9c626548d69913abdf2b9b51
baitemir <baitemir1706@gmail.com>
2023-11-06 22:01:18 +0600

    added select to dropdown

--- src/modules/Nav/Nav.jsx
+++ src/modules/Nav/Nav.jsx
@@ -2,10 +2,9 @@ import NavItem from "./NavItem/NavItem.jsx";
 import styles from "./Nav.module.scss"
 import Logo from "../../components/UI/Logo/Logo.jsx";
 import icon1 from "../../assets/icons/home.svg"
-
 const Nav = (props) => {
 
-
+    
     // eslint-disable-next-line react/prop-types
     const isActive = props.stateClassName;
 
@@ -14,7 +13,7 @@ const Nav = (props) => {
     return <nav className={navClass}>
         <Logo/>
         <ul>
-            <li><NavItem to={"/home"}> <img src={icon1} alt=""/> home</NavItem></li>
+            <li><NavItem to={"/"}> <img src={icon1} alt=""/> home</NavItem></li>
             <li><NavItem to={"/profile"}> <img src={icon1} alt=""/> profile</NavItem></li>
             <li><NavItem to={"/catalog"}> <img src={icon1} alt=""/> catalog</NavItem></li>
             <li><NavItem to={"/basket"}> <img src={icon1} alt=""/>basket</NavItem></li>
--- src/modules/SearchBar/SearchBar.jsx
+++ src/modules/SearchBar/SearchBar.jsx
@@ -1,8 +1,15 @@
 import styles from "./SearchBar.module.scss"
 import profile from "../../assets/icons/user.svg"
 import {Link} from "react-router-dom";
+import Select from "react-select";
 
 const SearchBar = (props) => {
+    const options = [
+        { label: 'Option 1', value: 'option1' },
+        { label: 'Option 2', value: 'option2' },
+        { label: 'Option 3', value: 'option3' },
+      ];
+      
     // eslint-disable-next-line react/prop-types
     const toggle  = props.toggleClassName;
     return <div className={styles.SearchBar}>
@@ -21,6 +28,10 @@ const SearchBar = (props) => {
                 ...
             </button>
             </section>
+        </div>
+        <div>
+            
+        <Select options={options} />    
         </div>
     </div>
 }
