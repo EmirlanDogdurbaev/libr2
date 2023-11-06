723d51f298005ccec2a9a5adc620e7f584c480aa
baitemir <baitemir1706@gmail.com>
2023-11-06 21:27:09 +0600

    fixed auth components

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,42 @@ import Layout from "./components/Layout/Layout.jsx";
 import { Route, Routes } from "react-router-dom";
 import Redirect from "./pages/Redirect/Redirect";
 import Home from "./pages/Home/Home";
-import AuthForm from './components/AuthForm/AuthForm.jsx'
+import AuthForm from "./components/AuthForm/AuthForm.jsx";
 import Login from "./pages/Auth/Login.jsx";
 import Register from "./pages/Auth/Register.jsx";
 function App() {
   return (
-    <Layout>
-      <Routes>
-        {!localStorage.getItem("token") ? (
-          <Route index element={<Home />} />
-        ) : (
-          <>
+    <>
+      {localStorage.getItem("token") ? (
+        <Layout>
+          <Routes>
+            <Route index element={<Home />} />
+          </Routes>
+        </Layout>
+      ) : (
+        <>
+          <Routes>
             <Route path="*" element={<Redirect />} />
-            <Route path="/login" element={<Login />} />
-          </>
-        )}
-      </Routes>
-    </Layout>
+            <Route
+              path="/login"
+              element={
+                <AuthForm>
+                  <Login />
+                </AuthForm>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <AuthForm>
+                  <Register />
+                </AuthForm>
+              }
+            />
+          </Routes>
+        </>
+      )}
+    </>
   );
 }
 
