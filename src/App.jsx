a59630900c5f013078f105990236c699d6d99d7c
EmirlanDogdurbaev <jigit38@gmail.com>
2023-11-06 20:45:23 +0600

    delete nessary functions

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,6 @@ function App() {
       {localStorage.getItem("token") ? (
         <Routes>
           <Route index path={"/home"} element={<Home />} />
-          <Route path={"/profile"} element={<Hello2 />} />
-          <Route path={"/catalog"} element={<Hello3 />} />
-          <Route path={"/basket"} element={<Hello4 />} />
         </Routes>
       ) : (
         <Routes>
@@ -26,14 +23,3 @@ function App() {
 }
 
 export default App;
-
-export const Hello2 = () => {
-  return <h1>hello 2</h1>;
-};
-
-export const Hello3 = () => {
-  return <h1>hello 3</h1>;
-};
-export const Hello4 = () => {
-  return <h1>hello 4</h1>;
-};
