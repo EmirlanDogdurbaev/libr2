d4b058342f675b1db2ed9c900079a4d98a6c6c1e
EmirlanDogdurbaev <jigit38@gmail.com>
2023-11-07 23:43:15 +0600

    adasdas

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,13 +1,8 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
 import "./BookSlider.scss";
-// import required modules
 import { Pagination } from "swiper/modules";
 import BookCard from "../BookCard/BookCard";
 
