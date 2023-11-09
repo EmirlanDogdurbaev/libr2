8e42d27eaa216952c3d1409d1c23f07724a046a4
EmirlanDogdurbaev <jigit38@gmail.com>
2023-11-09 23:10:51 +0600

    added slice in book

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -2,27 +2,35 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../../store/api";
-import cl from './Book.module.scss'
+import cl from "./Book.module.scss";
 import Stars from "../../components/Stars/Stars";
+
 export default function Book() {
   const params = useParams();
   const [book, setBook] = useState({});
-  console.log(typeof(book.rating));
+  const maxChars = 250; 
+  console.log(typeof book.rating);
+
   async function fetchBook() {
     try {
       const res = await axios.get(api + "/change/book/" + params.id + "/", {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
       });
       setBook(res.data);
     } catch (e) {
       console.log(e.message);
     }
   }
+
   useEffect(() => {
     fetchBook();
   }, []);
+
+  const rezka =
+    book && book.description ? book.description.slice(0, maxChars) : "";
+
   return (
     <div className={cl.Book}>
       <section>
@@ -32,14 +40,15 @@ export default function Book() {
             <h3>{book.title}</h3>
             <h4>{book.author}</h4>
             <p>
-              {book.description}
+              {book.description && book.description.length > 450
+                ? `${rezka}...`
+                : rezka}
             </p>
           </div>
           <div className={cl.rating}>
             <h3>{book.rating}</h3>
             <div>
-              
-            <Stars amount={book.rating} book={book} />
+              <Stars amount={book.rating} book={book} />
             </div>
             <div>
               <small>В наличии: 3</small>
