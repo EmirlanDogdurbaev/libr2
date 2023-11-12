74252667e2d4701545cc9c50e1b22c5d401ae217
baitemir <baitemir1706@gmail.com>
2023-11-12 21:45:55 +0600

    configured search bar logic

--- src/modules/SearchBar/SearchBar.jsx
+++ src/modules/SearchBar/SearchBar.jsx
@@ -1,45 +1,61 @@
-import styles from './SearchBar.module.scss';
-import profile from '../../assets/icons/user.svg';
-import { Link } from 'react-router-dom';
-import Select from 'react-select';
-import { useContext, useEffect, useState } from 'react';
-import {Context} from '../../main'
+import styles from "./SearchBar.module.scss";
+import { useLocation, useNavigate } from "react-router-dom";
+import Select from "react-select";
+import { useContext, useState } from "react";
+import { Context } from "../../main";
 const SearchBar = (props) => {
-  const {store} = useContext(Context)
-  const [search, setSearch] = useState('')
+  const { store } = useContext(Context);
+  const [search, setSearch] = useState("");
   const date = [
-    { label: 'Самые новые', value: 'newer' },
-    { label: 'Самые старые', value: 'older' },
+    { label: "Самые новые", value: "newer" },
+    { label: "Самые старые", value: "older" },
   ];
   const popularity = [
-    { label: 'Менее популярные', value: 'popular' },
-    { label: 'Более популярные', value: 'not' },
+    { label: "Менее популярные", value: "popular" },
+    { label: "Более популярные", value: "not" },
   ];
-  function filter(e){
-    store.setFilter(e.value)
-    store.setType('filter')
+  function filter(e) {
+    store.setFilter(e.value);
+    store.setType("filter");
   }
-  function submitSearch(){
-    store.setSearch(search)
-    setSearch('')
-    store.setType('search')
+  function submitSearch() {
+    store.setSearch(search);
+    setSearch("");
+    store.setType("search");
   }
- 
+
   const toggle = props.toggleClassName;
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
     <div className={styles.SearchBar}>
       <div>
-        <form action="#" onSubmit={(e)=>e.preventDefault()}>
-          <input type="text" placeholder="search for something... " value={search} onChange={(e)=>setSearch(e.target.value)} />
+        <form action="#" onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            placeholder="search for something... "
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onClick={()=>navigate('/catalog')}
+          />
           <button onClick={submitSearch}>search </button>
         </form>
 
-        <Select placeholder={'По дате'} options={date} onChange={(e)=>filter(e)}/>
-        <Select placeholder={'По популярности'} options={popularity}  onChange={(e)=>filter(e)}/>
+        {location.pathname === '/catalog' ? (
+          <>
+            <Select
+              placeholder={"По дате"}
+              options={date}
+              onChange={(e) => filter(e)}
+            />
+            <Select
+              placeholder={"По популярности"}
+              options={popularity}
+              onChange={(e) => filter(e)}
+            />
+          </>
+        ) : null}
         <section>
-          <Link to={'/reg'}>
-            <img src={profile} alt="" />
-          </Link>
           <button onClick={toggle}>...</button>
         </section>
       </div>
