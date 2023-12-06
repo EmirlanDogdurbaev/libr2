cce4e34308260fe442fd549dea91e7d2753a86fc
EmirlanDogdurbaev <jigit38@gmail.com>
2023-12-06 11:37:23 +0600

    added table in basket

--- src/components/BasketCard/BasketCard.jsx
+++ src/components/BasketCard/BasketCard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import BookCard from "../BookCard/BookCard";
 import Button from "../Button/Button";
-import classes from "./BasketCard.module.scss";
+import styles from "./BasketCard.module.scss";
 import axios from "axios";
 import { api } from "../../store/api";
 import { header } from "../../store/header";
@@ -10,7 +10,7 @@ export default function BasketCard({ item, fetchBook }) {
   const [book, setBook] = useState({});
   const [isLiber, setIsLiber] = useState(false);
 
-  console.log(item);
+  console.log(book);
   useEffect(() => {
     setIsLiber(JSON.parse(localStorage.getItem("user")).status === "Librarian");
     async function fetchData() {
@@ -49,35 +49,80 @@ export default function BasketCard({ item, fetchBook }) {
       console.log(e.message);
     }
   }
-  return (
-    <div className={classes.BasketCard}>
-      <div>
-        <div>{item.status}</div>
-        {isLiber ? (
-          <div className={classes.confirm_block}>
-            <div>
-              От : <span>{item.owner}</span>
-            </div>
 
-            {item.status === "Ожидает проверки" ? (
-              <section className={classes.btn_section}>
-                <Button action={confirm}>Подтвердить</Button>
-              </section>
-            ) : null}
-            {item.status === "Ожидает проверки" ? (
-              <section className={classes.btn_section}>
-                <Button action={rejectedOrder}>Удалить</Button>
-              </section>
-            ) : null}
-            {item.status === "В обработке" ? (
-              <section className={classes.btn_section}>
-                <Button action={giveOrder}>Выдать книгу</Button>
-              </section>
-            ) : null}
-          </div>
-        ) : null}
-      </div>
-      <BookCard data={book} />
+  return (
+    <div className={styles.tableContainer}>
+      <table className={styles.bookTable}>
+        <thead>
+          <tr>
+            <th>Автор</th>
+            <th>Название книги</th>
+            <th>Рейтинг книги</th>
+            <th>Количество книг</th>
+            <th>item.status</th>
+            <th>Действия</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr key={book.id}>
+            <td>{book.author}</td>
+            <td>{book.title}</td>
+            <td>{book.rating}</td>
+            <td>{book.quantity}</td>
+            <td>{item.status}</td>
+            <td>
+              {isLiber ? (
+                <div className={styles.confirm_block}>
+                  {item.status === "Ожидает проверки" ? (
+                    <section className={styles.btn_section}>
+                      <Button action={confirm}>Подтвердить</Button>
+                    </section>
+                  ) : null}
+                  {item.status === "Ожидает проверки" ? (
+                    <section className={styles.btn_section}>
+                      <Button action={rejectedOrder}>Удалить</Button>
+                    </section>
+                  ) : null}
+                  {item.status === "В обработке" ? (
+                    <section className={styles.btn_section}>
+                      <Button action={giveOrder}>Выдать книгу</Button>
+                    </section>
+                  ) : null}
+                </div>
+              ) : null}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
+
+// <div className={classes.BasketCard}>
+//   <div>
+//     <div>{item.status}</div>
+//     {isLiber ? (
+//       <div className={classes.confirm_block}>
+//         <div>
+//           От : <span>{item.owner}</span>
+//         </div>
+//         {item.status === "Ожидает проверки" ? (
+//           <section className={classes.btn_section}>
+//             <Button action={confirm}>Подтвердить</Button>
+//           </section>
+//         ) : null}
+//         {item.status === "Ожидает проверки" ? (
+//           <section className={classes.btn_section}>
+//             <Button action={rejectedOrder}>Удалить</Button>
+//           </section>
+//         ) : null}
+//         {item.status === "В обработке" ? (
+//           <section className={classes.btn_section}>
+//             <Button action={giveOrder}>Выдать книгу</Button>
+//           </section>
+//         ) : null}
+//       </div>
+//     ) : null}
+//   </div>
+// <BookCard data={book} />
+// </div>
